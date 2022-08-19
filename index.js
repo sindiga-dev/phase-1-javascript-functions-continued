ebab0addcac9436347cfe66622ceaf51d11ec32b
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-19 12:11:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+	return `This Saturday, I want to ${activity}!`;
+}
+// saturdayFun(`bathe my dog`);
+
+const mondayWork = function (activity = "go to the office") {
+	return `This Monday, I will ${activity}.`;
+};
+
+let wrapAdjective = function (valueAdjective = "*") {
+	return function (emphatic = "a hard worker") {
+		return `You are ${valueAdjective}${emphatic}${valueAdjective}!`;
+	};
+};
